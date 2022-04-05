6b60de2a4cfb58d7efd8a89ccc5db2d2a02c372d
Brian Moriguchi <brian@beckos.com>
2022-04-05 14:41:59 -0700

    Initial Commit

---
+++ SiwftUIApp/SiwftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiwftUIAppApp.swift
+//  SiwftUIApp
+//
+//  Created by Tatsuya Moriguchi on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SiwftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
