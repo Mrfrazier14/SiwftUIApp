6e21167f2b15a11794abd816413e0c59402709ef
Brian Moriguchi <brian@beckos.com>
2022-04-05 17:10:28 -0700

    Added items: [Data] for List

--- SiwftUIApp/ContentView.swift
+++ SiwftUIApp/ContentView.swift
@@ -7,13 +7,26 @@
 
 import SwiftUI
 
+struct Data: Identifiable {
+    var id = UUID()
+    let title: String
+    let imageName: String
+    let imageDescription: String
+    let imageTakenDate: String
+}
+
 struct ContentView: View {
+    
+    var items = [Data]()
+    
     var body: some View {
-        List(0..<10) {_ in
-            Image("apple")
-                .resizable()
+        List(items) { data in
+            Image(data.imageName)
+                .resizable().frame(width: 100, height: 100, alignment: .center)
+                .aspectRatio(contentMode: .fit)
+                
             
-            Text("Hello, world!")
+            Text(data.title)
                 .foregroundColor(.blue)
                 .fontWeight(.bold)
         }
@@ -23,7 +36,28 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(items: [
+            Data(title: "Apple",
+                 imageName: "apple",
+                 imageDescription: "This is an apple.",
+                 imageTakenDate: "1/1/2022"),
+            Data(title: "Banana",
+                 imageName: "banana",
+                 imageDescription: "This is a banana.",
+                 imageTakenDate: "1/1/2022"),
+            Data(title: "Blackberry",
+                 imageName: "blackberry",
+                 imageDescription: "This is a blackberry.",
+                 imageTakenDate: "1/1/2022"),
+            Data(title: "Blueberry",
+                 imageName: "blueberry",
+                 imageDescription: "This is a blueberry.",
+                 imageTakenDate: "1/1/2022"),
+            Data(title: "Cherry",
+                 imageName: "cherry",
+                 imageDescription: "This is a cherry.",
+                 imageTakenDate: "1/1/2022")
+        ])
             .previewDevice(PreviewDevice(rawValue: "iPhone 13"))
     }
 }
